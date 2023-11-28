60b8f7337ce339b51474fe59aa08f00858bf3a63
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-28 19:10:03 +0530

    variable practice

--- vars/vars.tf
+++ vars/vars.tf
@@ -16,29 +16,34 @@
 #    ]
 #  }
 #
-## how to access the list values
+## how to access the list values. list values always start with zero index.
 #output "list_values" {
 ##  value = var.list_values[0]
 #  value = var.list_values[1]
 #}
-#variable "map_values" {
-#  default = {
-#    apple = {
-#      stocks = 100
-#    }
-#    banana = {
-#      stocks = 100
-#    }
-#  }
-#}
-variable "map_values"  {
+variable "map_values" {
   default = {
-    apple = 100
-    banana = 200
+    apple = {
+      stocks = 100
+    }
+    banana = {
+      stocks = 100
+    }
   }
-
 }
+#variable "map_values"  {
+#  default = {
+#    apple = 100
+#    banana = 200
+#  }
+#
+#}
+#output "map_values" {
+#  value = var.map_values["apple"]
+#}
+# how to access the map values or list values along with strings
 output "map_values" {
-  value = var.map_values["apple"]
+ value = "apple stock is ${ var.map_values["apple"].stocks}"
 }
+
 
