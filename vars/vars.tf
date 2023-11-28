d6444c725c2fbe467b2186f981e50406962e2e66
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-28 18:57:41 +0530

    variable practice

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,6 +1,16 @@
+# plain variables
 variable "test" {
   default = "hello world"
 }
 output "test" {
   value = var.test
 }
+# list variables
+variable "list_values" {
+  default = [ "siva", "lingam" ]
+}
+# how to access the list values
+output "list_values" {
+  value = var.list_values[0]
+}
+
