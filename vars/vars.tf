0a65de2b3297c5c8363888f5a9b52448e35abdad
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-28 19:03:07 +0530

    variable practice

--- vars/vars.tf
+++ vars/vars.tf
@@ -1,24 +1,37 @@
 # plain variables
-variable "test" {
-  default = "hello world"
-}
-output "test" {
-  value = var.test
-}
+#variable "test" {
+#  default = "hello world"
+#}
+#output "test" {
+#  value = var.test
+#}
 # list variables
 #variable "list_values" {
 #  default = [ "siva", "lingam" ]
 #}
-variable "list_values" {
-    default = [
-      "siva",
-      "lingam"
-    ]
+#variable "list_values" {
+#    default = [
+#      "siva",
+#      "lingam"
+#    ]
+#  }
+#
+## how to access the list values
+#output "list_values" {
+##  value = var.list_values[0]
+#  value = var.list_values[1]
+#}
+variable "map_values" {
+  default = {
+    apple = {
+      stocks = 100
+    }
+    banana = {
+      stocks = 100
+    }
   }
-
-# how to access the list values
-output "list_values" {
-#  value = var.list_values[0]
-  value = var.list_values[1]
+}
+output "map_values" {
+  value = var.map_values["apple"].stock
 }
 
