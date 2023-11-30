006b57d187bb8b2a63a8cd592cf90f5a7a56a6d4
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-30 19:14:54 +0530

    for each practice

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -70,5 +70,5 @@ resource "aws_route53_record" "record" {
   name    = "${lookup(each.value, "name", null )}.sivadevops22.online"
   type    = "A"
   ttl     = 30
-  records = [lookup(aws_instance.instance, each.key["private"], null)]
+  records = [lookup(aws_instance.instance, each.key["private_ip"], null)]
 }
