4af83710c08d2bc05f838ebfb23b52c66a941e7e
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-30 18:46:18 +0530

    for each practice

---
+++ loops_for_each/main.tf
@@ -0,0 +1,20 @@
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend-dev"
+    }
+    backend = {
+      name = "backend-dev"
+    }
+
+  }
+}
+resource "aws_instance" "web" {
+  for_each = var.components
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = lookup(each.value, "name", null )
+  }
+}
